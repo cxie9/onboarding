8608f33186a4157b0fdca6a232628b687f51d5b6
Cindy <cxie9@hawk.iit.edu>
2016-02-08 14:28:45 -0600

    new project1 repo

---
+++ P1myscripts.js
@@ -0,0 +1,5 @@
+document.getElementById('home').addEventListener('click', function () {window.location.href = "Onboarding.html"; });
+
+document.getElementById('Oform').addEventListener('click', function () {window.location.href = "OnboardingForm.html"; });
+
+document.getElementById('butterfly').addEventListener('click', function () {window.location.href = "Butterfly.html"; });
