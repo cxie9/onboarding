837872132f92281011171218cef136e3e278d73d
Cindy <cxie9@hawk.iit.edu>
2016-02-08 16:51:49 -0600

    reworked whole project; deleted everything except main page; simplified

--- P1myscripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('home').addEventListener('click', function () {window.location.href = "Onboarding.html"; });
-
-document.getElementById('Oform').addEventListener('click', function () {window.location.href = "OnboardingForm.html"; });
-
-document.getElementById('butterfly').addEventListener('click', function () {window.location.href = "Butterfly.html"; });
