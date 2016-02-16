9d14ec135875358b4ddc91cb6fd7d21a4dfc2e98
Cindy <cxie9@hawk.iit.edu>
2016-02-16 00:55:39 -0600

    enabled submit button embedded alert with html fall-back & form validation

--- P1submit.js
+++ P1submit.js
@@ -1,10 +1,35 @@
-$("input[type=submit]").attr('disabled','disabled');
-$("form").change(function(){
-$("input[type=submit]").removeAttr('disabled');
+$("body").addClass("js");
+var pname = document.getElementById('pname');
+var telephone = document.getElementById('telephone');
+var email = document.getElementById('email');
+var pick = document.getElementById('pick');
+var form = document.getElementById('fields');
+var submit = document.getElementById('submit');
+var flag = false;
+form.addEventListener('keyup', function yolo(){
+    form.addEventListener('click', function swag(){
+        flag = false;
+        if(pname.value != ""){
+            if(telephone.value != ""){
+                if(email.value != ""){
+                    if(pick.value != ""){
+                        flag = true;
+                    }
+                }
+            }
+        }
+        if(flag == true){
+            submit.style.display = "inline"
+        } else {
+            submit.style.display = "none"
+        }
+    });
 });
 
-$(document).ready(function(){
-    $('form').submit(function(){
-        alert("Thank you for scheduling! We will confirm your appointment within 24 hours.");
-    });
+
+
+$('#fields').submit(function (ev){
+    ev.preventDefault();
+    document.getElementById('bottom').innerHTML = "Thank you for scheduling! We will confirm your appointment within 24 hours.";
+
 });
