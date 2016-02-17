93655ecb9fe01456639b73f532795ae167b04158
Cindy <cxie9@hawk.iit.edu>
2016-02-17 15:44:58 -0600

    Adding pages

---
+++ P1submit.js
@@ -0,0 +1,32 @@
+$("body").addClass("js");
+var pname = document.getElementById('pname');
+var email = document.getElementById('email');
+var pick = document.getElementById('pick');
+var form = document.getElementById('fields');
+var submit = document.getElementById('submit');
+var flag = false;
+form.addEventListener('keyup', function yolo(){
+    form.addEventListener('click', function swag(){
+        flag = false;
+        if(pname.value != ""){
+                if(email.value != ""){
+                    if(pick.value != ""){
+                        flag = true;
+                    }
+                }
+        }
+        if(flag == true){
+            submit.style.display = "inline"
+        } else {
+            submit.style.display = "none"
+        }
+    });
+});
+
+
+
+$('#fields').submit(function (ev){
+    ev.preventDefault();
+    document.getElementById('bottom').innerHTML = "Thank you for scheduling! We will confirm your appointment within 24 hours.";
+
+});
